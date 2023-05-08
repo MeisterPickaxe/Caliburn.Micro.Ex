fff0113f9afee02c38b2e882fb4f526a5e665679
MasterPickaxe <jpapion3401@gmail.com>
2023-05-08 17:31:01 +0900

    Add files via upload

---
+++ CMDEMO/README.md
@@ -0,0 +1,5 @@
+# Caliburn.Micro.Ex
+
+Tracing 
+
+https://www.youtube.com/watch?v=laPFq3Fhs8k
---
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,103 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private string _firstName = "Matte";
+        private string _lastName;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Matte", LastName = "Jeon" });
+            People.Add(new PersonModel { FirstName = "Alexa", LastName = "ndra" });
+            People.Add(new PersonModel { FirstName = "Timothy", LastName = "Salamae" });
+        }
+
+        public string FirstName
+        {
+            get 
+            {
+                return _firstName; 
+            }
+            set 
+            { 
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get { return $"{ FirstName } { LastName }"; }
+        }
+
+
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public  PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set 
+            { 
+                _selectedPerson = value; 
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public bool CanCleanText(string firstName, string lastName)
+        {
+            //return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+            // or ↓
+            if (String.IsNullOrWhiteSpace(firstName) && String.IsNullOrWhiteSpace(lastName))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void ClearText(string firstName, string lastName)
+        {
+            FirstName = "";
+            LastName = "";
+        }
+
+        public void LoadPageOne()
+        {
+            ActivateItemAsync(new FirstChildViewModel());
+        }
+
+        public void LoadPageTwo()
+        {
+            ActivateItemAsync(new SecondChildViewModel());
+        }
+    }
+}
